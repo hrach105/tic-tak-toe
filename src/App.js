a569a6fb7bc979ce2562f912ef6732d74bb04e01
hrach khachatryan <hrachkhachatryan24@gmail.com>
2020-05-07 19:08:44 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Game from './Components/Game';
+
+const App = () => {
+  return (
+    <div>
+      <Game />
+    </div>
+    
+  );
+}
+
+export default App;
